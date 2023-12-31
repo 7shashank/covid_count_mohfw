7525ca5eda0487d499b923fa9f273ac61a92abd1
7shashank <60084096+7shashank@users.noreply.github.com>
2023-12-31 16:00:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3002; // You can choose any available port
+const port = process.env.PORT || 3002; // You can choose any available port
 
 
 // Replace 'https://example.com/api/data' with the actual URL containing JSON data
